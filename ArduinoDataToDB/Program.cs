4eb2a0f2ba8e053cf04cd809f8b34e9ad943e234
Stefan Rompen <54891722+sRompen@users.noreply.github.com>
2022-04-04 19:17:34 +0200

    Add project files.

---
+++ ArduinoDataToDB/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ ---IMPORTANT---
+It is important to read the README attached to this project on GitHub.
+When using this code, you need to set the right serialport, in my case it was
+COM7 (see line 24), but this will be different on every laptop/computer. Also,
+you will need to make a connection to your own MySQL db. A manual about connecting 
+your own database can be found in the README file. 
+
+GitHub link: 
+*/ 
+
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ArduinoDataToDB
+{
+    class Program
+    {
+        static SerialPort _serialPort;
+        public static void Main()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM7"; //Set your board COM
+            _serialPort.BaudRate = 9600;
+            _serialPort.Open();
+            while (true)
+            {
+                string record = _serialPort.ReadExisting();
+                Console.WriteLine(record);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
